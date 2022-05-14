eb72c24adda78c42fb3be985663320041df34f0a
xiaokang00010 <zhouyongkang4321@gmail.com>
2022-05-14 20:09:02 +0800

    Added some command implementation to XScript2

--- Executor/BytecodeInterpreter.cpp
+++ Executor/BytecodeInterpreter.cpp
@@ -81,14 +81,224 @@ namespace XScript {
                     }
                     break;
                 }
-                case BytecodeStructure::InstructionEnum::calculation_sub:
+                case BytecodeStructure::InstructionEnum::calculation_sub: {
+                    auto Right = InterpreterEnvironment.Stack.PopValueFromStack();
+                    auto Left = InterpreterEnvironment.Stack.PopValueFromStack();
+                    switch (Left.Kind) {
+                        case EnvironmentStackItem::ItemKind::Integer:
+                            switch (Right.Kind) {
+                                case EnvironmentStackItem::ItemKind::Integer:
+                                    Left.Value.IntVal -= Right.Value.IntVal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Decimal:
+                                    Left.Value.DeciVal = static_cast<XDecimal>(Left.Value.IntVal) - Right.Value.DeciVal;
+                                    Left.Kind = EnvironmentStackItem::ItemKind::Decimal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Boolean:
+                                    Left.Value.IntVal -= static_cast<XInteger>(Right.Value.BoolVal);
+                                    break;
+                                case EnvironmentStackItem::ItemKind::HeapPointer:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a object.");
+                                case EnvironmentStackItem::ItemKind::Null:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a null value.");
+                            }
+                            InterpreterEnvironment.Stack.PushValueToStack(Left);
+                            break;
+                        case EnvironmentStackItem::ItemKind::Decimal:
+                            switch (Right.Kind) {
+                                case EnvironmentStackItem::ItemKind::Integer:
+                                    Left.Value.DeciVal = Left.Value.DeciVal - static_cast<XDecimal>(Right.Value.IntVal);
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Decimal:
+                                    Left.Value.DeciVal -= Right.Value.DeciVal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Boolean:
+                                    Left.Value.DeciVal -= static_cast<XDecimal>(Right.Value.BoolVal);
+                                    break;
+                                case EnvironmentStackItem::ItemKind::HeapPointer:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a object.");
+                                case EnvironmentStackItem::ItemKind::Null:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a null value.");
+                            }
+                            InterpreterEnvironment.Stack.PushValueToStack(Left);
+                            break;
+                        case EnvironmentStackItem::ItemKind::Boolean:
+                            switch (Right.Kind) {
+                                case EnvironmentStackItem::ItemKind::Integer:
+                                    Left.Value.IntVal = static_cast<XInteger>(Left.Value.BoolVal) - Right.Value.IntVal;
+                                    Left.Kind = EnvironmentStackItem::ItemKind::Integer;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Decimal:
+                                    Left.Value.DeciVal =
+                                            static_cast<XDecimal>(Left.Value.BoolVal) - Right.Value.DeciVal;
+                                    Left.Kind = EnvironmentStackItem::ItemKind::Decimal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Boolean:
+                                    Left.Value.BoolVal -= Right.Value.BoolVal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::HeapPointer:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a object.");
+                                case EnvironmentStackItem::ItemKind::Null:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a null value.");
+                            }
+                            InterpreterEnvironment.Stack.PushValueToStack(Left);
+                            break;
+                        case EnvironmentStackItem::ItemKind::HeapPointer:
+                            break;
+                        case EnvironmentStackItem::ItemKind::Null:
+                            throw BytecodeInterpretError(L"Cannot add elements with a null value.");
+                    }
                     break;
-                case BytecodeStructure::InstructionEnum::calculation_mul:
+                }
+                case BytecodeStructure::InstructionEnum::calculation_mul: {
+                    auto Right = InterpreterEnvironment.Stack.PopValueFromStack();
+                    auto Left = InterpreterEnvironment.Stack.PopValueFromStack();
+                    switch (Left.Kind) {
+                        case EnvironmentStackItem::ItemKind::Integer:
+                            switch (Right.Kind) {
+                                case EnvironmentStackItem::ItemKind::Integer:
+                                    Left.Value.IntVal *= Right.Value.IntVal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Decimal:
+                                    Left.Value.DeciVal = static_cast<XDecimal>(Left.Value.IntVal) * Right.Value.DeciVal;
+                                    Left.Kind = EnvironmentStackItem::ItemKind::Decimal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Boolean:
+                                    Left.Value.IntVal *= static_cast<XInteger>(Right.Value.BoolVal);
+                                    break;
+                                case EnvironmentStackItem::ItemKind::HeapPointer:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a object.");
+                                case EnvironmentStackItem::ItemKind::Null:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a null value.");
+                            }
+                            InterpreterEnvironment.Stack.PushValueToStack(Left);
+                            break;
+                        case EnvironmentStackItem::ItemKind::Decimal:
+                            switch (Right.Kind) {
+                                case EnvironmentStackItem::ItemKind::Integer:
+                                    Left.Value.DeciVal = Left.Value.DeciVal * static_cast<XDecimal>(Right.Value.IntVal);
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Decimal:
+                                    Left.Value.DeciVal *= Right.Value.DeciVal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Boolean:
+                                    Left.Value.DeciVal *= static_cast<XDecimal>(Right.Value.BoolVal);
+                                    break;
+                                case EnvironmentStackItem::ItemKind::HeapPointer:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a object.");
+                                case EnvironmentStackItem::ItemKind::Null:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a null value.");
+                            }
+                            InterpreterEnvironment.Stack.PushValueToStack(Left);
+                            break;
+                        case EnvironmentStackItem::ItemKind::Boolean:
+                            switch (Right.Kind) {
+                                case EnvironmentStackItem::ItemKind::Integer:
+                                    Left.Value.IntVal = static_cast<XInteger>(Left.Value.BoolVal) * Right.Value.IntVal;
+                                    Left.Kind = EnvironmentStackItem::ItemKind::Integer;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Decimal:
+                                    Left.Value.DeciVal =
+                                            static_cast<XDecimal>(Left.Value.BoolVal) * Right.Value.DeciVal;
+                                    Left.Kind = EnvironmentStackItem::ItemKind::Decimal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Boolean:
+                                    Left.Value.BoolVal *= Right.Value.BoolVal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::HeapPointer:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a object.");
+                                case EnvironmentStackItem::ItemKind::Null:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a null value.");
+                            }
+                            InterpreterEnvironment.Stack.PushValueToStack(Left);
+                            break;
+                        case EnvironmentStackItem::ItemKind::HeapPointer:
+                            break;
+                        case EnvironmentStackItem::ItemKind::Null:
+                            throw BytecodeInterpretError(L"Cannot add elements with a null value.");
+                    }
                     break;
-                case BytecodeStructure::InstructionEnum::calculation_div:
+                }
+                case BytecodeStructure::InstructionEnum::calculation_div: {
+                    auto Right = InterpreterEnvironment.Stack.PopValueFromStack();
+                    auto Left = InterpreterEnvironment.Stack.PopValueFromStack();
+                    switch (Left.Kind) {
+                        case EnvironmentStackItem::ItemKind::Integer:
+                            switch (Right.Kind) {
+                                case EnvironmentStackItem::ItemKind::Integer:
+                                    Left.Value.IntVal /= Right.Value.IntVal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Decimal:
+                                    Left.Value.DeciVal = static_cast<XDecimal>(Left.Value.IntVal) / Right.Value.DeciVal;
+                                    Left.Kind = EnvironmentStackItem::ItemKind::Decimal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Boolean:
+                                    Left.Value.IntVal /= static_cast<XInteger>(Right.Value.BoolVal);
+                                    break;
+                                case EnvironmentStackItem::ItemKind::HeapPointer:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a object.");
+                                case EnvironmentStackItem::ItemKind::Null:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a null value.");
+                            }
+                            InterpreterEnvironment.Stack.PushValueToStack(Left);
+                            break;
+                        case EnvironmentStackItem::ItemKind::Decimal:
+                            switch (Right.Kind) {
+                                case EnvironmentStackItem::ItemKind::Integer:
+                                    Left.Value.DeciVal = Left.Value.DeciVal / static_cast<XDecimal>(Right.Value.IntVal);
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Decimal:
+                                    Left.Value.DeciVal /= Right.Value.DeciVal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Boolean:
+                                    Left.Value.DeciVal /= static_cast<XDecimal>(Right.Value.BoolVal);
+                                    break;
+                                case EnvironmentStackItem::ItemKind::HeapPointer:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a object.");
+                                case EnvironmentStackItem::ItemKind::Null:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a null value.");
+                            }
+                            InterpreterEnvironment.Stack.PushValueToStack(Left);
+                            break;
+                        case EnvironmentStackItem::ItemKind::Boolean:
+                            switch (Right.Kind) {
+                                case EnvironmentStackItem::ItemKind::Integer:
+                                    Left.Value.IntVal = static_cast<XInteger>(Left.Value.BoolVal) / Right.Value.IntVal;
+                                    Left.Kind = EnvironmentStackItem::ItemKind::Integer;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Decimal:
+                                    Left.Value.DeciVal =
+                                            static_cast<XDecimal>(Left.Value.BoolVal) / Right.Value.DeciVal;
+                                    Left.Kind = EnvironmentStackItem::ItemKind::Decimal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::Boolean:
+                                    Left.Value.BoolVal *= Right.Value.BoolVal;
+                                    break;
+                                case EnvironmentStackItem::ItemKind::HeapPointer:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a object.");
+                                case EnvironmentStackItem::ItemKind::Null:
+                                    throw BytecodeInterpretError(L"Cannot add integers with a null value.");
+                            }
+                            InterpreterEnvironment.Stack.PushValueToStack(Left);
+                            break;
+                        case EnvironmentStackItem::ItemKind::HeapPointer:
+                            break;
+                        case EnvironmentStackItem::ItemKind::Null:
+                            throw BytecodeInterpretError(L"Cannot add elements with a null value.");
+                    }
                     break;
-                case BytecodeStructure::InstructionEnum::calculation_mod:
+                }
+                case BytecodeStructure::InstructionEnum::calculation_mod: {
+                    auto Right = InterpreterEnvironment.Stack.PopValueFromStack();
+                    auto Left = InterpreterEnvironment.Stack.PopValueFromStack();
+                    if (Left.Kind != EnvironmentStackItem::ItemKind::Integer or
+                        Right.Kind != EnvironmentStackItem::ItemKind::Integer) {
+                        throw BytecodeInterpretError(L"Command calculation_mod only can be used between 2 integers.");
+                    }
+                    Left.Value.IntVal %= Right.Value.IntVal;
+                    InterpreterEnvironment.Stack.PushValueToStack(Left);
                     break;
+                }
                 case BytecodeStructure::InstructionEnum::logic_and:
                     break;
                 case BytecodeStructure::InstructionEnum::logic_or:
---
+++ README.md
@@ -0,0 +1,14 @@
+### XScript 2
+
+
+XScript 2 is the Next Generation XScript Programming Language.
+
+### The goal of XScript 2
+
+- [ ] Create a bytecode-based virtual machine for XScript2
+- [ ] Create a standard library for XScript 2
+- [ ] Implement a full package system for XScript 2
+
+### Developing state
+
+- Add some basic structures to XScript 2 (May/14/2022)
