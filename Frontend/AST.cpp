3a50b3e1c5a61b53d04d2be4447b014c90169331
xiaokang00010 <zhouyongkang4321@gmail.com>
2022-07-21 11:58:55 +0800

    Fixed bugs on compiling

--- Frontend/AST.cpp
+++ Frontend/AST.cpp
@@ -5,17 +5,14 @@
 #include "AST.hpp"
 
 namespace XScript {
-    AST::AST() : Type(TreeType::NotMatch), TypeSpecifierArrayDimensionsCount(0) {}
+    AST::AST() : Type(TreeType::NotMatch), {}
 
     AST::AST(AST::TreeType Type, Lexer::Token Node, XArray<AST> Subtrees) : Type(Type), Node(std::move(Node)),
-                                                                            Subtrees(std::move(Subtrees)),
-                                                                            TypeSpecifierArrayDimensionsCount(0) {}
+                                                                            Subtrees(std::move(Subtrees)), {}
 
-    AST::AST(AST::TreeType Type, Lexer::Token Node) : Type(Type), Subtrees(), Node(std::move(Node)),
-                                                      TypeSpecifierArrayDimensionsCount(0) {}
+    AST::AST(AST::TreeType Type, Lexer::Token Node) : Type(Type), Subtrees(), Node(std::move(Node)), {}
 
-    AST::AST(AST::TreeType Type, XArray<AST> Subtrees) : Type(Type), Node(), Subtrees(std::move(Subtrees)),
-                                                         TypeSpecifierArrayDimensionsCount(0) {}
+    AST::AST(AST::TreeType Type, XArray<AST> Subtrees) : Type(Type), Node(), Subtrees(std::move(Subtrees)), {}
 
     XBoolean AST::IsLeafNode() const {
         return Subtrees.empty();
